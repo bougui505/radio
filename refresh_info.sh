276a9219f7aa846130f187324dd7cf5a635e0aea
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-04-29 15:43:25 +0200

    new file:   refresh_info.sh

---
+++ refresh_info.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2020-04-29 14:43:39 (UTC+0200)
+
+while :; do
+    tmux send-keys -t bottom R
+    sleep 10
+done
