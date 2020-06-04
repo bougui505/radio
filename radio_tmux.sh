23423b744b3e2e28b8699136f37a863f45b50578
Guillaume - Maud - Malo <bougui@localhost.localdomain>
2020-06-04 10:22:22 +0200

    Attach to existing tmux session if radio session exists, else create it 	modified:   radio_tmux.sh

--- radio_tmux.sh
+++ radio_tmux.sh
@@ -6,5 +6,8 @@
 # 2020-04-17 12:24:28 (UTC+0200)
 
 DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
-
-tmux new-session -s radio $DIRSCRIPT/radio.sh
+if ( tmux has-session -t radio 2>/dev/null ); then
+    tmux attach -t radio
+else
+    tmux new-session -s radio $DIRSCRIPT/radio.sh
+fi
