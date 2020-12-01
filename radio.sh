4cffa2084e6dc6616c01dea31b6a9eed22a9ffd6
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-12-01 09:43:40 +0100

    Remove the hard coded path to fzf

--- radio.sh
+++ radio.sh
@@ -8,7 +8,7 @@ DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
 
 function radio_menu () {
     cat $DIRSCRIPT/radio_list.txt\
-        | /home/bougui/source/fzf/bin/fzf --tac --with-nth=1
+        | fzf --tac --with-nth=1
 }
 
 while :; do
